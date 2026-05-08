138237d4c6c42a2fd2b91663945a9e790813f4c3
Gonzalo25U <gonzalo.s.ulloa@gmail.com>
2026-05-08 16:08:53 -0400

    Implementacion de la automatizacion de las citas medicas + Remplazo del RestTemplate por WebClient

--- appointment-service/src/main/java/com/rednorte/appointment_service/client/UserClient.java
+++ appointment-service/src/main/java/com/rednorte/appointment_service/client/UserClient.java
@@ -1,33 +1,26 @@
 package com.rednorte.appointment_service.client;
 
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
-@Service
+@Component
+@RequiredArgsConstructor
 public class UserClient {
 
-    private final RestTemplate restTemplate;
-
-    public UserClient(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
+    private final WebClient webClient;
 
     public boolean existsUser(String rut) {
         try {
-            String url = "http://user-service:8081/users/rut/" + rut;
-            restTemplate.getForObject(url, Object.class);
+            webClient.get()
+                    .uri("http://user-service:8081/users/rut/" + rut)
+                    .retrieve()
+                    .bodyToMono(Object.class)
+                    .block();
             return true;
-
-        } catch (HttpClientErrorException e) {
-
-            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
-                return false;
-            }
-
-            throw new RuntimeException("Error en user-service: " + e.getStatusCode());
-
+        } catch (WebClientResponseException.NotFound e) {
+            return false;
         } catch (Exception e) {
             throw new RuntimeException("Error conectando con user-service");
         }
