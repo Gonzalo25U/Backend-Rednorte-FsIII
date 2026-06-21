87fd1ac6d4e6ceb6400f96247ca9795cc703145e
Gonzalo25U <gonzalo.s.ulloa@gmail.com>
2026-06-21 15:21:15 -0400

    Fix bff

--- bff-service/src/main/java/com/rednorte/bff_service/config/SecurityConfig.java
+++ bff-service/src/main/java/com/rednorte/bff_service/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers(
                     "/swagger-ui/**",
                     "/swagger-ui.html",
